8cae482b584a4ea3855662c0c1e88f138d551646
Xses <wiktort52@gmail.com>
2024-06-11 18:31:05 +0200

    It's dijkstrin'

--- src/dijkstra.pas
+++ src/dijkstra.pas
@@ -6,13 +6,22 @@ var
 	sources		: charlist;
 	destinations	: charlist;
 	weights		: list;
+	origin		: char;
+	destination	: char;
 
 	space		: char;
 	counter		: integer;
+	
+	processed_vert	: list;
+	distances	: list;
+	smallest_edge	: integer;
 begin
 	counter := 0;
 	sources[-1] := 'A';
 	space := ' ';
+
+	writeln('Provide the input in the format separated by spaces and with the new line');
+	writeln('between each entry of the edge. After the last line write ! as EOF');
 
 	{ Inputing the weighted edge list }
 	while sources[counter - 1] <> '!' do
@@ -25,8 +34,25 @@ begin
 		counter := counter + 1;
 	end;
 
+	writeln('Now provide the origin and destination vertices in one line');
+	readln(origin, destination);
 
-	{ Outputing the list for a test }
+	
+	{ Dijkstra here }
+	{ Initialize the set }
+	counter		:= 0;
+	distances[0]	:= 0;
+	while sources[counter] <> '!' do
+	begin
+		processed_vert[counter] := 0;
+		distances[counter + 1] := 99999999;
+	end;
+
+	{ Findign the closest vertices and processing them }
+
+
+
+
 	counter := 0;
 	while sources[counter - 1] <> '!' do
 	begin
